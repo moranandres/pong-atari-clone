d41a55c50e80d909b926a2a73f527b5a7594cbfa
Andrés Moran <amoranc8@est.ups.edu.ec>
2025-06-15 22:09:23 -0500

    Feat: renderizar paleta del jugador

--- js/game.js
+++ js/game.js
@@ -1,8 +1,23 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+// 1) Paleta del jugador
+const paddle = {
+  width: 10,
+  height: 100,
+  x: 10,
+  y: canvas.height / 2 - 50,
+  speed: 5
+};
+
+function drawPaddle() {
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPaddle();
   requestAnimationFrame(gameLoop);
 }
 
