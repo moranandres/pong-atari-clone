59a74345084b1ca9abc1fe2f6a0245a5e495cb55
Andrés Moran <amoranc8@est.ups.edu.ec>
2025-06-15 21:23:04 -0500

    Feat: agregar js/game.js con dibujo inicial de paleta y bucle de juego

---
+++ js/game.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Configuración de la paleta
+const paddle = {
+  width: 10,
+  height: 100,
+  x: 10,
+  y: canvas.height / 2 - 50,
+  speed: 5
+};
+
+function drawPaddle() {
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPaddle();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
